e212c9ae01f46d806851d468f0627219442f9b36
Bikram Sahoo <bikram.sahoo@waiin.com>
2023-08-14 19:04:12 +0530

    api created

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]/")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _dataContext;
+
+    public UsersController(DataContext dataContext){
+        //TODO: Add DB connection here
+        _dataContext = dataContext;
+    }
+
+    [HttpGet]  // get/users
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUses(){
+        var users = await _dataContext.Users.ToListAsync();
+        
+        return users;
+    }
+
+    [HttpGet("{id}")] //get/users/2
+    public async Task<ActionResult<AppUser>> GetUserById(int id){
+        return await _dataContext.Users.FindAsync(id);
+    }
+
+}
